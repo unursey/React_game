6c86021365361b6e7534b741938b63b9045a17ed
anna <unursey@gmail.com>
2022-10-17 22:03:12 +0300

    edit

--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -4,31 +4,37 @@ import PropTypes from 'prop-types';
 
 export class ClassComponent extends React.Component {
   state = {
-    result: 'Результат',
+    isGameOver: true,
+    result: `Введите число от ${this.props.min} до ${this.props.max}`,
     userNum: '',
-    randomNum: Math.floor(Math.random() * this.props.max - this.props.min) +
-      this.props.min,
+    randomNum: Math.floor(Math.random() *
+    (this.props.max - this.props.min + 1)) + this.props.min,
     count: 0,
   };
 
-  changeVisibility = (sid) => {
-    const elem = document.getElementById(sid);
-    let typedisp = window.getComputedStyle(elem, null).display;
-    typedisp = typedisp === 'none' ? 'flex' : 'none';
-    elem.style.display = typedisp;
-  };
+  // changeVisibility = (sid) => {
+  //   const elem = document.getElementById(sid);
+  //   let typedisp = window.getComputedStyle(elem, null).display;
+  //   typedisp = typedisp === 'none' ? 'flex' : 'none';
+  //   elem.style.display = typedisp;
+  // };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleSubmit = () => {
     this.setState(state => ({
       count: state.count + 1,
     }));
 
     this.setState(state => {
       if (!state.userNum) {
-        return {
-          result: `Введите число`,
-        };
+        if (!state.count < 0) {
+          return {
+            count: 0,
+          };
+        } else {
+          return {
+            count: state.count - 1,
+          };
+        }
       }
 
       if (state.userNum > state.randomNum) {
@@ -43,9 +49,8 @@ export class ClassComponent extends React.Component {
         };
       }
 
-      this.changeVisibility('form');
-      this.changeVisibility('game');
       return {
+        isGameOver: false,
         result: `Вы угадали, это число ${state.userNum},
         попыток ${state.count}`,
       };
@@ -65,13 +70,12 @@ export class ClassComponent extends React.Component {
   };
 
   startNewGame = () => {
-    this.changeVisibility('form');
-    this.changeVisibility('game');
     this.setState(() => ({
-      result: 'Результат',
+      isGameOver: true,
+      result: `Введите число от ${this.props.min} до ${this.props.max}`,
       count: 0,
-      randomNum: Math.floor(Math.random() * this.props.max - this.props.min) +
-      this.props.min,
+      randomNum: Math.floor(Math.random() *
+      (this.props.max - this.props.min + 1)) + this.props.min,
     }));
   };
 
@@ -80,20 +84,28 @@ export class ClassComponent extends React.Component {
       <div className={style.game}>
         <p className={style.result}>{this.state.result}</p>
 
-        <form className={style.form} id='form' onSubmit={this.handleSubmit}>
-          <label className={style.label} htmlFor='user_number'>
-            Угадай число
-          </label>
+        <form className={style.form} id='form' onSubmit={(e) =>
+          e.preventDefault()}>
+          {this.state.isGameOver ? (
+            <>
+              <label className={style.label} htmlFor='user_number'>
+                Угадай число
+              </label>
 
-          <input className={style.input} type='number' id='user_number'
-            onChange={this.handleChange} value={this.state.userNum}/>
+              <input className={style.input} type='number' id='user_number'
+                onChange={this.handleChange} value={this.state.userNum}/>
 
-          <button className={style.btn}>Угадать</button>
+              <button className={style.btn} onClick={this.handleSubmit}>
+                Угадать
+              </button>
+            </>
+          ) : (
+            <button className={style.btn} id='game'
+              onClick={this.startNewGame}>
+              Сыграть ещё
+            </button>
+          )}
         </form>
-        <button className={style.btn} id='game' style={{display: 'none'}}
-          onClick={this.startNewGame}>
-          Сыграть ещё
-        </button>
       </div>
     );
   }
